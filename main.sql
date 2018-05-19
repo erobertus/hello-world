7bfc1c31f401bb3ad13c84c50444ac103ae59de3
erobertus <34409507+erobertus@users.noreply.github.com>
2018-05-19 15:50:54 -0400

    Revert "d"
    This reverts commit bd3910c19e61b701791d72fbde80aded96be35b9, reversing
    changes made to 44a363215d66beacefddfaf73a222be633217449.

--- main.sql
+++ main.sql
@@ -3,14 +3,9 @@ select * from db_option where option_id = 2
 
 use master;
 
-<<<<<<< HEAD
 /**************************************
 The code below is for information only
 **************************************/
 
 select * from master.information
 select * from oscar.info 
-=======
--- now we need to explore
--- also this branch up
->>>>>>> readme-edits
