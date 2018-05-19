416f74acd450a84f5ffd89337dbb4040db34d86c
erobertus <34409507+erobertus@users.noreply.github.com>
2018-05-19 17:04:03 -0400

    conflict resolved

--- main.sql
+++ main.sql
@@ -8,5 +8,5 @@ The code below is for information only
 **************************************/
 
 select * from master.information
-select * from oscar.info 
+select * from oscar.info where oscar_id in (select oscar_id from oscar_public where published)
 select 1;
